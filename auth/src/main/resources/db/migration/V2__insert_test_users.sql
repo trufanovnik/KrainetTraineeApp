8e5ce4a0f0b385a083a81b6051a8957d3f5c9ebc
unknown <trufanovwork@gmail.com>
2025-08-29 08:53:06 +0300

    test data for users table with flyway V2 migration

---
+++ auth/src/main/resources/db/migration/V2__insert_test_users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, password, email, role_type) VALUES
+('user1', '$2a$12$wDXoxtzOOD17qzONHU2tBe0xHkWvcyAD41.P9Ekti8Bzg4Kemiune', 'user1@kr.com', 'USER'),
+('user2', '$2a$12$TSB0GFFUL5ev3DOKeHy.O.eNiFNLyc6ujnkmYR3hxjy3qKnmmezAe', 'user2@kr.com', 'USER'),
+('user3', '$2a$12$Bislo6akvf4HAQUCYRGoEe.7/Bc2lrRDFVdYHIjlS2iBiX5QIYj0q', 'user3@kr.com', 'USER'),
+('user4', '$2a$12$8nJcIQXa3mJCiSjHA4GTUejrBmkrxa30nnOZqkHqXRvP9JP.riC6O', 'user4@kr.com', 'USER'),
+('admin1', '$2a$12$mfGxTkl7XPA5CJaGz0cnTuIMxtKuwEG4GFo6EBgHA30Nsct500h7O', 'admin1@kr.com', 'ADMIN'),
+('admin2', '$2a$12$x1uWmrMrIohnW.cy3wzUN.nyJBlkjwVSTOTUfPzQVGojcIO.HaoBW', 'admin2@kr.com', 'ADMIN');
