97e3ba88143c6b7daf10ed3f39ac7d45fa60d917
unknown <trufanovwork@gmail.com>
2025-08-29 18:09:16 +0300

    create user repository with find/existByusername methods

---
+++ auth/src/main/java/by/krainet/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package by.krainet.auth.repository;
+
+import by.krainet.auth.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+    boolean existByUsername(String username);
+}
