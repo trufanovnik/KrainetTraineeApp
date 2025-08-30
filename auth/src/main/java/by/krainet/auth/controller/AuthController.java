d661af0a6b155378c8049d8486aa96162c07b405
unknown <trufanovwork@gmail.com>
2025-08-30 12:43:56 +0300

    auth controller with register method

---
+++ auth/src/main/java/by/krainet/auth/controller/AuthController.java
@@ -0,0 +1,22 @@
+package by.krainet.auth.controller;
+
+import by.krainet.auth.dto.RegistrationRequest;
+import by.krainet.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegistrationRequest req) {
+        return authService.register(req);
+    }
+}
