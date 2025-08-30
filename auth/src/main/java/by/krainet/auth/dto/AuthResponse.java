7e617bda804499efed651de5f0ae85028b162d7c
unknown <trufanovwork@gmail.com>
2025-08-30 12:54:18 +0300

    constructor

--- auth/src/main/java/by/krainet/auth/dto/AuthResponse.java
+++ auth/src/main/java/by/krainet/auth/dto/AuthResponse.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 @Data
 public class AuthResponse {
+
+    public AuthResponse(String token, String refreshToken) {
+        this.token = token;
+        this.refreshToken = refreshToken;
+    }
+
     private String token;
     private String refreshToken;
 }
