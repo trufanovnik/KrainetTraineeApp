b7857d003840405960dd01a2e86466633ec7a7bb
unknown <trufanovwork@gmail.com>
2025-09-01 13:01:05 +0300

    cleaned up the log output

--- auth/src/main/java/by/krainet/auth/config/UserSecurity.java
+++ auth/src/main/java/by/krainet/auth/config/UserSecurity.java
@@ -16,19 +16,15 @@ public class UserSecurity {
 
     public boolean isOwnerOrAdmin(Long id, Authentication authentication) {
         String role = authentication.getAuthorities().iterator().next().getAuthority();
-        System.out.println("Role: " + role);
         if ("ROLEADMIN".equals(role)) {
             return true;
         }
         Object principal = authentication.getPrincipal();
         if (principal instanceof UserDetails userDetails) {
             String username = userDetails.getUsername();
-            System.out.println("Login from JWT: " + username);
             UserEntity user = userRepository.findByUsername(username).orElseThrow(
                     () -> new EntityNotFoundException("Пользователь не найден"));
-            System.out.println("User from DB: " + user);
             if (user != null) {
-                System.out.println("User id from DB: " + user.getId());
                 return user.getId().equals(id);
             }
         }
